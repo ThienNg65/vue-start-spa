e09cf2aec78bc0820148f8de49ad5387aa83156b
Thien Nguyen <nvtt1510@gmail.com>
2023-10-06 12:21:42 +0700

    starting from scratch

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav
+    :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-sm']"
+  >
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Vue</a>
+      <!-- Links -->
+      <ul class="navbar-nav">
+        <li v-for="(page, index) in pages" class="nav-item" :key="index">
+          <a
+            class="nav-link"
+            :class="{ active: activePage == index }"
+            aria-current="page"
+            :href="page.link.url"
+            :title="`This link goes to the ${page.link.text} page`"
+            @click.prevent="navLinkClick(index)"
+            >{{ page.link.text }}
+          </a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <button class="btn btn-primary" @click.prevent="changeTheme()">
+          Toggle
+        </button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ["pages", "activePage", "navLinkClick"],
+
+  data() {
+    return {
+      theme: "light",
+    };
+  },
+  methods: {
+    changeTheme() {
+      this.theme = this.theme === "light" ? "dark" : "light";
+    },
+  },
+};
+</script>
